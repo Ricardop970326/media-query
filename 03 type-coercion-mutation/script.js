c175456fb883d0b251e6550514ffbe66923c964e
Ricardop970326 <ricardop.alejandrog@gmail.com>
2023-10-04 14:14:15 -0400

    first commit

---
+++ 03 type-coercion-mutation/script.js
@@ -0,0 +1,69 @@
+//variable Mutation;
+
+let firstName = "Ricardo";
+let age = 22;
+
+
+age = 26;
+firstName = "Richard Alexander";
+// console.log firstName and age;
+
+//type coercion;
+let message = "My age is: " + age;
+console.log(message);
+console.log(typeof message);
+
+// Aritmetic operation 
+let result = "10" - 5;
+console.log(result, typeof result);
+
+// strinbg coercion
+let value = true;
+value = String(value);
+alert(typeof value)
+
+//numeric conversion;
+//alert("6"/"2");
+
+let str = "123";
+let num = Number(str);
+// alert(typeof num);
+
+//
+let amount = Number("an arbitrary value intead of a number");
+//alert(amount)
+
+// more convercion
+// true come out 1 and false as 0
+
+//alert(Number(true));
+//alert(Number(false));
+
+//boolean conversion
+//alert(Boolean(1));
+//alert(Boolean(0));
+
+
+//alert(Boolean("hello"));
+//alert(Boolean(""));
+
+let shoes = null;
+
+//if (shoes == false ) && (shoes == ) {
+    //alert("shoes evaluated as true")
+
+//} else {
+   // console.log("shoes evaluated as false it's type  is", typeof shoes )
+//} 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ responsive/README.md
@@ -0,0 +1,15 @@
+# R2H-Responsive-web-design
+
+This activity is designed for students to practice with media queries.
+
+For this activity 
+1. Clone the repository onto your computer
+2. Open in VS code and web browser
+3. Observe how the code works as is
+
++ Resize the window to be smaller. Notice how the color and font size changes as the window gets smaller
+
+4. Continue the trend for the red and yellow
+
++ The Green should show at 1000px
++ The Yellow should show at 700px
